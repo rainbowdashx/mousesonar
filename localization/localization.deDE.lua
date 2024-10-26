b4fb69f907d0ba8c269893e5c6f24ce066195144
rainbowdashx <born_to_kill_666@hotmail.com>
2024-10-27 00:57:56 +0200

    update DE

--- localization/localization.deDE.lua
+++ localization/localization.deDE.lua
@@ -1,18 +1,18 @@
 if GetLocale() ~= "deDE" then return end
 
 local L = {}
-L["Disable Mouse Sonar"] = "Maus Sonar deaktivieren"
+L["Disable Mouse Sonar"] = "Maussonar deaktivieren"
 L["Always Show Circle"] = "Kreis immer anzeigen"
-L["Keep Circle Visible During Mouselook"] = "Kreis während Kameramodus sichtbar halten"
+L["Keep Circle Visible During Mouselook"] = "Kreis während Mausblick sichtbar halten"
 L["Show Circle Only in Combat"] = "Kreis nur im Kampf anzeigen"
-L["Show Circle Only in Raid/Party"] = "Kreis nur in Raid/Gruppe anzeigen"
-L["Show Circle When Mouselook Ends"] = "Kreis anzeigen, wenn Kameramodus endet"
+L["Show Circle Only in Raid/Party"] = "Kreis nur in Schlachtzug/Gruppe anzeigen"
+L["Show Circle When Mouselook Ends"] = "Kreis anzeigen, wenn Mausblick endet"
 L["Enable Mouse Shake Detection"] = "Maus-Schüttel-Erkennung aktivieren"
-L["Display Hollow Circle Style"] = "Hohlen Kreisstil anzeigen"
+L["Display Hollow Circle Style"] = "Hohlen Kreis-Stil anzeigen"
 L["Circle Size"] = "Kreisgröße"
-L["Initial Circle Opacity"] = "Anfängliche Kreisopazität"
+L["Initial Circle Opacity"] = "Anfängliche Kreisdeckkraft"
 L["Circle Color"] = "Kreisfarbe"
 L["Mouse Shake Sensitivity"] = "Maus-Schüttel-Empfindlichkeit"
-L["Bind or use /pulse command for manual pulse."] = "Binde oder verwende /pulse für manuelles Pulsen."
+L["Bind or use /pulse command for manual pulse."] = "Binde oder verwende den Befehl /pulse für manuelles Auslösen."
 
 MouseSonar_Locale = L
